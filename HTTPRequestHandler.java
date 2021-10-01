f6cfd0c57abe372b1c544881e2e16953df62ffce
Fernandez de Lara Dylan <dyf3@zoo.cs.yale.edu>
2021-10-01 15:28:21 -0400

    partial header implementations

--- HTTPRequestHandler.java
+++ HTTPRequestHandler.java
@@ -3,6 +3,10 @@
  **/
 import java.io.*;
 import java.net.*;
+import java.net.Authenticator.RequestorType;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 class HTTPRequestHandler {
@@ -10,22 +14,37 @@ class HTTPRequestHandler {
     static boolean _DEBUG = true;
     static int     reqCount = 0;
 
+	static boolean isModifiedSinceFlag = false;
+	String isModifiedSinceDate;
+
+	long lastModifiedDate;
+
     String WWW_ROOT;
     Socket connSocket;
+	HashMap<String, String> serverCache;
+	int maxServerCacheSize;
+	HashMap<String, String> configMap;
+
     BufferedReader inFromClient;
     DataOutputStream outToClient;
 
     String urlName;
+	String hostName;
     String fileName;
     File fileInfo;
 
+	Integer statusCode = 200;
+
     public HTTPRequestHandler(Socket connectionSocket, 
-			     String WWW_ROOT) throws Exception
+			     String WWW_ROOT, HashMap<String, String> serverCache, int maxServerCacheSize, HashMap<String, String> configMap) throws Exception
     {
         reqCount ++;
 
 	    this.WWW_ROOT = WWW_ROOT;
 	    this.connSocket = connectionSocket;
+		this.serverCache = serverCache;
+		this.maxServerCacheSize = maxServerCacheSize;
+		this.configMap = configMap;
 
 	    inFromClient =
 	      new BufferedReader(new InputStreamReader(connSocket.getInputStream()));
@@ -65,7 +84,7 @@ class HTTPRequestHandler {
 	    // process the request
 	    String[] request = requestMessageLine.split("\\s");
 		
-	    if (request.length < 2 || !request[0].equals("GET"))
+	    if (request.length < 2 || !request[0].equals("GET") || !request[2].equals("HTTP/1.1"))
 	    {
 		    outputError(500, "Bad request");
 		    return;
@@ -79,11 +98,46 @@ class HTTPRequestHandler {
 
         // debugging
         if (_DEBUG) {
-           String line = inFromClient.readLine();
-           while ( !line.equals("") ) {
-              DEBUG( "Header: " + line );
-              line = inFromClient.readLine();
+        	String line = inFromClient.readLine();
+			request = line.split("\\s");
+
+        	while ( !line.equals("") ) {
+            	DEBUG( "Header: " + line );
+				
+				// Host Request
+				if (request.length != 2 && !request[0].equals("HOST:")){
+					outputError(500, "Bad request");
+		    		return;
+				}
+				hostName = request[1];
+				if (!configMap.get(hostName)){
+					outputError(404, "Host Not Found");
+		    		return;
+				}
+				WWW_ROOT = configMap.get(hostName);
+
+				// User-Agent Request
+            	line = inFromClient.readLine();
+				request = line.split("\\s");
+
+				// If-Modified-Since Request
+				line = inFromClient.readLine();
+				request = line.split("\\s");
+
+				if (request[0].equals("If-Modified-Since:")){
+					isModifiedSinceFlag = true;
+
+					for (int i = 1; i < request.length; i++){
+						isModifiedSinceDate.concat(request[i]);
+						if (i != request.length - 1){
+							isModifiedSinceDate.concat(" ");
+						}
+					}
+
+				}
+
            }
+
         }
 
 	    // map to file name
@@ -96,16 +150,31 @@ class HTTPRequestHandler {
 		    outputError(404,  "Not Found");
 		    fileInfo = null;
 	    }
+		// fileName exists as a file
+		else {
+			lastModifiedDate = fileInfo.lastModified();
+		}
 
     } // end mapURL2file
 
 
     private void outputResponseHeader() throws Exception 
     {
-	    outToClient.writeBytes("HTTP/1.0 200 Document Follows\r\n");
-	    outToClient.writeBytes("Set-Cookie: MyCool433Seq12345\r\n");
+		String statusMsg = returnStatusMessage(statusCode);
 
-	    if (urlName.endsWith(".jpg"))
+		// HTTP/1.1 Header
+	    outToClient.writeBytes("HTTP/1.0 " + statusCode.toString() + statusMsg);
+		
+		// Date Header
+		outToClient.writeBytes(LocalDateTime.now());
+
+		// Server Header
+		outToClient.writeBytes(hostName);
+
+		// Last-Modified Header
+
+		// Content-Type Header
+		if (urlName.endsWith(".jpg"))
 	        outToClient.writeBytes("Content-Type: image/jpeg\r\n");
 	    else if (urlName.endsWith(".gif"))
 	        outToClient.writeBytes("Content-Type: image/gif\r\n");
@@ -113,20 +182,33 @@ class HTTPRequestHandler {
 	        outToClient.writeBytes("Content-Type: text/html\r\n");
 	    else
 	        outToClient.writeBytes("Content-Type: text/plain\r\n");
+
+		// Content Length Header
+
+		// part1 doesn't have anything with cookies as of right now
+	    outToClient.writeBytes("Set-Cookie: temporaryCookie\r\n");
+
     }
 
     private void outputResponseBody() throws Exception 
     {
-
 	    int numOfBytes = (int) fileInfo.length();
 	    outToClient.writeBytes("Content-Length: " + numOfBytes + "\r\n");
 	    outToClient.writeBytes("\r\n");
 	
 	    // send file content
-	    FileInputStream fileStream  = new FileInputStream (fileName);
-	
-	    byte[] fileInBytes = new byte[numOfBytes];
-	    fileStream.read(fileInBytes);
+	    FileInputStream fileStream = new FileInputStream (fileName);
+		byte[] fileInBytes = new byte[numOfBytes];
+
+		if (!serverCache.containsKey(fileName)){
+			fileStream.read(fileInBytes);
+			if ((serverCache.size() + numOfBytes) < maxServerCacheSize) {
+				serverCache.put(fileName, fileInBytes);
+			}
+		}
+		else {
+			fileInBytes = serverCache.get(fileName);
+		}
 
 	    outToClient.write(fileInBytes, 0, numOfBytes);
     }
@@ -143,4 +225,13 @@ class HTTPRequestHandler {
        if (_DEBUG)
           System.out.println( s );
     }
+
+	String returnStatusMessage(Integer code){
+		// will add more codes in next iteration
+		String msg = "Invalid code";
+		if (code == 200){
+			msg = " OK\r\n";
+		}
+		return msg;
+	}
 }
--- HTTPServer.java
+++ HTTPServer.java
@@ -9,26 +9,69 @@ import java.util.*;
 class HTTPServer{
 
     public static int serverPort = 6789;    
-    //public static String WWW_ROOT = "/home/httpd/html/zoo/classes/cs434/";
-    public static String WWW_ROOT = "./";
+    public static String WWW_ROOT = "/home/httpd/html/zoo/classes/cs434/";
+    //public static String WWW_ROOT = "./";
 
     public static void main(String args[]) throws Exception  {
-	
-	// see if we do not use default server port
-	if (args.length >= 1)
-	    serverPort = Integer.parseInt(args[0]);
 
-	// see if we want a different root
-	if (args.length >= 2)
-	    WWW_ROOT = args[1];
+	// creates map 
+	HashMap<String, String> configArgs = new HashMap<String, String>();
+
+	if (args[0].equals("-config")){
+		// sets config values
+		Scanner parser = new Scanner(new File(args[1]));
+		String tempRoot = "";
+		String tempServerName = "";
+			
+		String iterator = parser.next();
+		
+		// parse to set values
+		while (parser.hasNext()) {
+			if (iterator.equals("Listen")){
+				iterator = parser.next();
+				configArgs.put("Port", iterator);
+			}
+			else if (iterator.equals("CacheSize")){
+				iterator = parser.next();
+				configArgs.put("CacheSize", iterator);
+			}
+			else if (iterator.equals("DocumentRoot")){
+				iterator = parser.next();
+				tempRoot = iterator;
+			}
+			else if (iterator.equals("ServerName")){
+				iterator = parser.next();
+				tempServerName = iterator;
+				configArgs.put(tempServerName, tempRoot);
+			}
+			iterator = parser.next();
+		}
+
+		serverPort = Integer.valueOf(configArgs.get("Port"));
+	}
+	else {
+		// see if we do not use default server port
+		if (args.length >= 1 )
+			serverPort = Integer.parseInt(args[0]);
+
+		// see if we want a different root
+		if (args.length >= 2)
+			WWW_ROOT = args[1];
+	}
 
 	// create server socket
 	ServerSocket listenSocket = new ServerSocket(serverPort);
 	System.out.println("server listening at: " + listenSocket);
 	System.out.println("server www root: " + WWW_ROOT);
 
+	// create server cache - stores static files
+	HashMap<String, String> serverCache = new HashMap<String, String>();
+
+	// set max cache size
+	int maxServerCacheSize = Integer.valueOf(configArgs.get("CacheSize"));
+
 	while (true) {
-		
+
 	    try {
 
 		    // take a ready connection from the accepted queue
@@ -37,7 +80,7 @@ class HTTPServer{
 	
 		    // process a request
 		    HTTPRequestHandler wrh = 
-		        new HTTPRequestHandler( connectionSocket, WWW_ROOT );
+		        new HTTPRequestHandler(connectionSocket, WWW_ROOT, serverCache, maxServerCacheSize, configArgs);
 
 		    wrh.processRequest();
 
@@ -46,6 +89,38 @@ class HTTPServer{
 		}
 	} // end of while (true)
 	
-    } // end of main
+} // end of main
+
+public static void parseConfigFile(String configFile, HashMap<String, String> map) throws FileNotFoundException{
+	try (Scanner parser = new Scanner(new FileInputStream(configFile))){
+		String tempRoot = "";
+		String tempServerName = "";
+		
+		String iterator = parser.next();
+
+		while (parser.hasNext()) {
+			if (iterator.equals("Listen")){
+				iterator = parser.next();
+				map.put("Port", iterator);
+			}
+			else if (iterator.equals("CacheSize")){
+				iterator = parser.next();
+				map.put("CacheSize", iterator);
+			}
+			else if (iterator.equals("DocumentRoot")){
+				iterator = parser.next();
+				tempRoot = iterator;
+			}
+			else if (iterator.equals("ServerName")){
+				iterator = parser.next();
+				tempServerName = iterator;
+				map.put(tempServerName, tempRoot);
+			}
+			iterator = parser.next();
+		}
+	}
+	catch (FileNotFoundException exception){
+	}
+}
 
 } // end of class WebServer
